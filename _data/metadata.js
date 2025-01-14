37f68b2543c3f29b9c5f1a99d89c1af37950d0a4
Margaret Fero <MaggieFero@users.noreply.github.com>
2025-01-13 21:18:21 -0800

    Update metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Margaret Fero's Website",
+	url: "https://maggiefero.github.io/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am building a website and I haven't decided what will be on it yet.",
 	author: {
-		name: "Your Name Here",
+		name: "Margaret Fero",
 		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		url: "https://maggiefero.github.io/"
 	}
 }
